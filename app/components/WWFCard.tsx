a984e21c76f866a25825a03afe7f8159a8b97a0d
jennaly <106183040+jennaly@users.noreply.github.com>
2024-06-11 16:03:52 -0700

    feat(ui): style what we fund section and components

---
+++ app/components/WWFCard.tsx
@@ -0,0 +1,36 @@
+import { figtree } from "@/lib/fonts";
+import Image from "next/image";
+import React from "react";
+
+const WWFCard = () => {
+  return (
+    <div className="w-full md:w-[48%] xl:w-[32%] flex flex-col py-[24px] border-t xl:border-b">
+      <h3 className={`${figtree.className} text-lg md:text-xl xl:text-2xl`}>
+        Innovative and promising research
+      </h3>
+      <div className="w-full pt-[24px]">
+        <Image
+          src="/whatWeFund-program1.webp"
+          alt="program 1"
+          width="300"
+          height="500"
+          className="w-full h-128 object-cover object-center rounded-xl"
+        />
+      </div>
+
+      <div
+        className={`${figtree.className} text-md md:text-lg xl:text-xl pt-[24px]`}
+      >
+        <span className="font-semibold">
+          We fund research at institutions nationwide.
+        </span>{" "}
+        <p>
+          Bridge funding gaps for research projects by helping researchers prove
+          their ideas and secure larger grants.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WWFCard;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { HomePage } from "@/lib/definitions";
 import { crimson, figtree } from "@/lib/fonts";
 import Image from "next/image";
 import DonateCTA from "./components/DonateCTA";
+import CTA from "./components/CTA";
+import WWFCard from "./components/WWFCard";
 
 export default async function Home() {
   const data: HomePage[] = await getHomePage();
@@ -22,7 +24,7 @@ export default async function Home() {
         // style={{
         //   backgroundImage: `url(${heroSection.backgroundImg.desktopImg.imageURL})`,
         // }}
-        className={`relative flex min-h-screen flex-col items-start justify-start py-24 bg-[url('/hero-desktop.webp')] bg-cover bg-[60%] md:bg-center xl:items-center xl:justify-center border-2 border-red-400`}
+        className={`relative flex min-h-screen flex-col items-start justify-start py-24 bg-[url('/hero-desktop.webp')] bg-cover bg-[60%] md:bg-center xl:items-center xl:justify-center`}
       >
         <div
           id="hero-backgroundOverlay"
@@ -30,7 +32,7 @@ export default async function Home() {
         />
         <div
           id="hero-textOverlay"
-          className="absolute z-20 left-10 right-10 bottom-10 md:bottom-16 lg:bottom-20 flex flex-col gap-4 border-2 border-red-700 max-w-[1440px] md:w-[450px] lg:w-[680px] xl:relative xl:mt-144 xl:mr-144 text-white"
+          className="absolute z-20 left-[32px] md:left-10 right-[32px] md:right-10 bottom-10 md:bottom-16 lg:bottom-20 flex flex-col gap-4 xl:gap-8 max-w-[1440px] md:w-[450px] lg:w-[680px] xl:relative xl:mt-144 xl:mr-96 text-white"
         >
           <h1
             className={`${crimson.className} text-4xl md:text-5xl lg:text-6xl xl:text-7xl`}
@@ -43,6 +45,65 @@ export default async function Home() {
           </p>
 
           <DonateCTA />
+        </div>
+      </section>
+
+      <section
+        id="mission"
+        className="px-[32px] pt-[80px] lg:pt-[160px] max-w-[1440px] mx-auto flex flex-col lg:flex-row lg:items-stretch gap-8 lg:justify-between"
+      >
+        <div
+          id="mission-text"
+          className="w-full lg:w-2/5 flex flex-col gap-4 lg:justify-around lg:gap-0"
+        >
+          <h2
+            className={`${crimson.className} text-3xl md:text-4xl lg:text-5xl xl:text-6xl`}
+          >
+            We are on a mission to end childhood cancer.
+          </h2>
+          <p
+            className={`${figtree.className} text-lg lg:text-xl md:my-4 xl:my-0`}
+          >
+            Children’s Cancer Research Fund is a national nonprofit dedicated to
+            ending childhood cancer. Our main focus is to support the research
+            of bright scientists across the country whose ideas can make the
+            greatest impact for children fighting cancer.
+          </p>
+
+          <CTA />
+        </div>
+
+        <div id="mission-image" className="w-full lg:w-1/2">
+          <Image
+            src="/mission-image.webp"
+            width="500"
+            height="500"
+            alt="mission image"
+            className="w-full h-full object-cover rounded-xl"
+          />
+        </div>
+      </section>
+
+      <section
+        id="whatWeFund"
+        className="px-[32px] pt-[80px] lg:pt-[160px] max-w-[1440px] mx-auto flex flex-col"
+      >
+        <div id="whatWeFund-header" className="mb-[40px] lg:mb-[80px]">
+          <h2
+            className={`${crimson.className} text-3xl md:text-4xl lg:text-5xl xl:text-6xl`}
+          >
+            We design, build and run collaborative spaces where climate-science,
+            technology and people can thrive
+          </h2>
+        </div>
+
+        <div
+          id="whatWeFund-programs"
+          className="flex flex-col md:flex-row md:flex-wrap md:justify-between"
+        >
+          <WWFCard />
+          <WWFCard />
+          <WWFCard />
         </div>
       </section>
     </main>
