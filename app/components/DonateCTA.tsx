3d00860f601b3f300d7ae01b5d74ae7c8ef095eb
jennaly <jenna@wicot.io>
2024-06-10 20:38:32 -0700

    feat(ui): create donate button component

---
+++ app/components/DonateCTA.tsx
@@ -0,0 +1,14 @@
+import { figtree } from "@/lib/fonts";
+import React from "react";
+
+const DonateCTA = () => {
+  return (
+    <button
+      className={`${figtree.className} bg-ccrf-green rounded-full w-fit px-4 py-2`}
+    >
+      Donate Now
+    </button>
+  );
+};
+
+export default DonateCTA;
