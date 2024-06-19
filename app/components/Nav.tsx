a30e1c67fe70373c3310fb20867fba767b9113fc
jennaly <106183040+jennaly@users.noreply.github.com>
2024-06-19 15:30:45 -0700

    fix(ui): span full viewport width

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="w-full max-w-[1440px] mx-auto px-[32px] xl:px-[64px] py-[20px] fixed top-0 bg-white z-50 ">
-      <div className="">
+    <nav className="w-full fixed top-0 bg-white z-50 ">
+      <div className="max-w-[1440px] mx-auto px-[32px] xl:px-[64px] py-[20px]">
         <Image
           src="/ccrf-logo-narrow-blue.png"
           width="160"
